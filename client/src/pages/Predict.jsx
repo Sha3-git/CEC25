a67576fe7627b5703246ac379a7df470746ff5f5
Shema Dabiri <dabirishema@gmail.com>
2025-03-15 13:33:06 -0300

    fix

--- client/src/pages/Predict.jsx
+++ client/src/pages/Predict.jsx
@@ -2,7 +2,7 @@
 export default function Predict() {
   return (
     <>
-    <div className="container mt-auto">
+    <div className="container"style={{ height: "100vh" }}>
         Hello
     </div>
     </>
